860a1f6a8a0b2332781170e3ed7e64bb7515c527
tales <talestales@gmail.com>
2020-08-20 18:01:53 -0300

    a

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const cli = require('./lib/cli');
 
 cli.run();
+
+// Grande parte do código foi feito a partir de https://github.com/tryber/sd-03-a-cli-of-ice-and-fire/pull/2 , https://github.com/tryber/sd-03-a-cli-of-ice-and-fire/pull/16 //
